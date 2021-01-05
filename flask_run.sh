adf9687e5c0a4017ba41183f09ffb6c19dd72296
Quentin Joder <quentinjoder@gmail.com>
2021-01-05 12:03:29 +0100

    Hello World

---
+++ flask_run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Script created to ease my work in PyCharm Community Edition
+export FLASK_APP=server.py
+export FLASK_ENV=$1
+flask run
---
+++ server.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello_world(name=None):
+    return render_template('index.html', name=name)
